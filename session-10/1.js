23d3f95f78d3f824a3031117a90a9912cbc7e35f
vinodbaret <vinodsondliya@gmail.com>
2023-03-29 15:37:15 +0530

    j1

--- session-10/1.js
+++
@@ -1,6 +0,0 @@
-var a = 10;
-var b = 10;
-
-var c = a + b;
-
-console.log("SUM IS " + c);
---
+++ session-10/variable.js
@@ -0,0 +1,8 @@
+var name = "vinod"
+var age = 22
+var location = 1.2222
+isactive = true
+console.log("name",name)
+console.log("age",age)
+console.log("location",location)
+console.log("is active on social media",isactive)
